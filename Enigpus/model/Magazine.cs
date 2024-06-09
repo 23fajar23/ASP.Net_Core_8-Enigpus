bb2f2f9d4a3f02dee791fdcf267765c5d71ef52d
23fajar23 <fajardarussalam20@gmail.com>
2024-06-09 07:47:52 +0700

    [PART] make model and abstract

---
+++ Enigpus/model/Magazine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigpus.model
+{
+    public class Magazine : Book
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public int PublicationYear { get; set; }
+        public override string GetTitle()
+        {
+            return Title;
+        }
+    }
+}
---
+++ Enigpus/model/Novel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigpus.model
+{
+    public class Novel : Book
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        public override string GetTitle()
+        {
+            return Title;
+        }
+    }
+}
